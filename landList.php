2a929ab7857bdf50c95b803fd07547dd64e49ab8
rshielajoy <regisshielajoy@gmail.com>
2019-10-31 01:02:38 +0800

    search results

---
+++ landList.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title></title>
+        <link rel="stylesheet" href="stylesheet/result.css" />
+        <link rel="stylesheet" href="stylesheet/header.css" />
+        <link rel="stylesheet" href="stylesheet/landlist.css" />
+    </head>
+
+    <body>
+        <div class="header">
+            <!-- <a href="#default" class="logo">  -->
+                <img src="img/agrilogo.png" width="65" height="50">
+            <!-- </a> -->
+            <div class="header-right">
+                <a class="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        </div>
+
+        <div id="landlist">
+            <h3 id="land"> SEARCH RESULTS </h3>
+            <div id="land_con1">
+               <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+
+            <div id="land_con2">
+                <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+
+            <div id="land_con3">
+                <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+
+            <div id="land_con4">
+                <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+
+            <div id="land_con5">
+                <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+
+            <div id="land_con6">
+                <img src="img/lot.png" width="300px" height="150px" style="padding-left: 50px;"> <br>
+               <label style="padding-left: 10px;">  Address: </label> <br>
+               <label style="padding-left: 10px;">  Owner: ***** ***** ******* </label> <br>
+               <label  style="padding-left: 10px;"> For sale </label> <br>
+            </div>
+        </div>
+
+        <div id="contact-info"> 
+            <center> <h3> SEARCH </h3> </center>
+            
+            <div id="use_box">
+                <input type="text" id="search" name="search" placeholder="search land areas">
+                <select id="category" name="category">
+                    <option value="public">public</option>
+                    <option value="for lease">for lease</option>
+                    <option value="for sale">for sale</option>
+                    <option value="Government owned">Government owned</option>
+		        </select>
+                <center><input type = "submit" id="search_button" name = "Seacrh" value = "Search"> <center>
+            </div>
+        </div>
+
+    </body>
+</html>
